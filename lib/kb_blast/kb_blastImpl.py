f746baddad33d821c294980451d3c32109ce8aca
Dylan Chivian <DCChivian@lbl.gov>
2016-08-03 15:35:29 -0700

    debugging

--- lib/kb_blast/kb_blastImpl.py
+++ lib/kb_blast/kb_blastImpl.py
@@ -69,7 +69,29 @@ class kb_blast:
 
     def translate_nuc_to_prot_seq (self, nuc_seq, table='11'):
         prot_seq = ''
-        prot_seq = 'ACDEFGHIKLMNPQRSTVWY'
+        if table != '11':
+            raise ValueError ("error in translate_nuc_to_prot_seq.  Only know genetic code table 11 (value used '%s')"%table)
+
+        genetic_code = dict()
+        genetic_code['11'] = {
+            'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+            'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+            'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+            'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+            'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+            'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+            'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+            'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+            'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+            'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+            'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+            'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+            'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+            'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+            'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'
+            }
+        prot_seq = ''.join([genetic_code[table].get(nuc_seq[3*i:3*i+3],'X') for i in range(len(nuc_seq)//3)])
 
         return prot_seq
 
@@ -1581,6 +1603,7 @@ class kb_blast:
                                 else:
                                     protein_sequence_found_in_many_input = True
                                     protein_translation = self.translate_nuc_to_prot_seq (feature['dna_sequence'], table='11')
+                                    console.log("protein_seq: %s"%protein_translation)
                                     record = SeqRecord(Seq(protein_translation), id=feature['id'], description=genome['id'])
                                     records.append(record)
                                     
