9a4250bc72880e784a983cab4f6c43e9ec935973
Dylan Chivian <DCChivian@lbl.gov>
2016-08-18 13:29:44 -0700

    should be a get from os.environ?

--- lib/kb_blast/kb_blastImpl.py
+++ lib/kb_blast/kb_blastImpl.py
@@ -380,7 +380,7 @@ class kb_blast:
         self.shockURL = config['shock-url']
         self.handleURL = config['handle-service-url']
 #        self.callbackURL = os.environ['SDK_CALLBACK_URL'] if os.environ['SDK_CALLBACK_URL'] != None else 'https://kbase.us/services/njs_wrapper'
-        self.callbackURL = os.environ['SDK_CALLBACK_URL']
+        self.callbackURL = os.environ.get('SDK_CALLBACK_URL')
         if self.callbackURL == None:
             raise ValueError ("SDK_CALLBACK_URL not set in environment")
         self.scratch = os.path.abspath(config['scratch'])
