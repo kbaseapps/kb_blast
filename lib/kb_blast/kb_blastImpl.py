65b1d4311d7abd742d316d6ca5269c554e782bb2
Dylan Chivian <dcchivian@lbl.gov>
2017-02-03 19:49:45 -0800

    added serviceWizardURL

--- lib/kb_blast/kb_blastImpl.py
+++ lib/kb_blast/kb_blastImpl.py
@@ -3012,6 +3012,7 @@ class kb_blast:
             #output_featureSet['element_ordering'] = []
             output_featureSet['elements'] = dict()
             for fid in feature_ids:
+                self.log(console, "FID: '"+str(fid)+"'")  # DEBUG
                 seq_total += 1
                 try:
                     in_filtered_set = hit_seq_ids[fid]
