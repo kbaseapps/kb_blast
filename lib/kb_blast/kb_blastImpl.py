62f8138a8f01c1d5ceb2e468e83e3006db46d651
Dylan Chivian <dcchivian@lbl.gov>
2017-02-03 19:39:27 -0800

    added serviceWizardURL

--- lib/kb_blast/kb_blastImpl.py
+++ lib/kb_blast/kb_blastImpl.py
@@ -199,6 +199,7 @@ class kb_blast:
         self.workspaceURL = config['workspace-url']
         self.shockURL = config['shock-url']
         self.handleURL = config['handle-service-url']
+        self.serviceWizardURL = config['service-wizard-url']
 
 #        self.callbackURL = os.environ['SDK_CALLBACK_URL'] if os.environ['SDK_CALLBACK_URL'] != None else 'https://kbase.us/services/njs_wrapper'
         self.callbackURL = os.environ.get('SDK_CALLBACK_URL')
