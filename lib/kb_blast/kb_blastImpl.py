2aa9534494fe4d5117ccc59f23cd72a8dcef53bb
dcchivian <dcchivian@lbl.gov>
2018-05-04 15:58:48 -0700

    prepping to fix bug and updated base docker image

--- lib/kb_blast/kb_blastImpl.py
+++ lib/kb_blast/kb_blastImpl.py
@@ -53,9 +53,9 @@ class kb_blast:
     # state. A method could easily clobber the state set by another while
     # the latter method is running.
     ######################################### noqa
-    VERSION = "1.0.4"
+    VERSION = "1.0.5"
     GIT_URL = "https://github.com/kbaseapps/kb_blast"
-    GIT_COMMIT_HASH = "f663a9f17a89886826f4f2ff2d9c211db4318384"
+    GIT_COMMIT_HASH = "b38a71615efecdbc018075bb54b398115a497fae"
 
     #BEGIN_CLASS_HEADER
     workspaceURL = None
