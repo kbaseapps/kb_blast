24048d5fa77e932a5672a73b9764004d951113de
dcchivian <dcchivian@lbl.gov>
2022-04-25 20:38:54 +0000

    v1.7.0 - cleaning up LGTM warnings

--- lib/kb_blast/Utils/BlastUtil.py
+++ lib/kb_blast/Utils/BlastUtil.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import gzip
 import os
 import re
 import subprocess
@@ -9,14 +8,7 @@ import uuid
 from datetime import datetime
 from pprint import pformat
 
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-import requests
-from requests_toolbelt import MultipartEncoder
-
 # SDK Utils
-from installed_clients.AbstractHandleClient import AbstractHandle
 from installed_clients.KBaseDataObjectToFileUtilsClient import KBaseDataObjectToFileUtils
 from installed_clients.kb_SetUtilitiesClient import kb_SetUtilities
 from installed_clients.DataFileUtilClient import DataFileUtil as DFUClient
@@ -84,13 +76,13 @@ class BlastUtil:
         self.handleURL = config['handle-service-url']
         self.serviceWizardURL = config['service-wizard-url']
 
-#        self.callbackURL = os.environ['SDK_CALLBACK_URL'] if os.environ['SDK_CALLBACK_URL'] != None else 'https://kbase.us/services/njs_wrapper'
+#        self.callbackURL = os.environ['SDK_CALLBACK_URL'] if os.environ['SDK_CALLBACK_URL'] is not None else 'https://kbase.us/services/njs_wrapper'
         self.callbackURL = os.environ.get('SDK_CALLBACK_URL')
-        if self.callbackURL == None:
+        if self.callbackURL is None:
             raise ValueError ("SDK_CALLBACK_URL not set in environment")
 
         self.scratch = os.path.abspath(config['scratch'])
-        if self.scratch == None:
+        if self.scratch is None:
             self.scratch = os.path.join('/kb','module','local_scratch')
         if not os.path.exists(self.scratch):
             os.makedirs(self.scratch)
@@ -134,7 +126,7 @@ class BlastUtil:
         PROT_pattern = re.compile("^[acdefghiklmnpqrstvwyACDEFGHIKLMNPQRSTVWYxX ]+$")
         NUC_pattern  = re.compile("^[acgtuACGTUnryNRY ]+$")   
 
-        if header_id == None:  header_id = 'N/A'
+        if header_id is None:  header_id = 'N/A'
 
         if seq_type.startswith('NUC'):
             if not NUC_pattern.match(sequence_str):
@@ -171,20 +163,15 @@ class BlastUtil:
                 raise ValueError('input_msa_ref parameter is required')
 
         else:  # need to store textarea query
-            if ('output_one_name' not in params or params['output_one_name'] == None) \
-                and ('input_one_sequence' in params and params['input_one_sequence'] != None):
+            if params.get('output_one_name') is None and params.get('input_one_sequence') is not None:
                 raise ValueError('output_one_name parameter required if input_one_sequence parameter is provided')
-            if ('output_one_name' in params and params['output_one_name'] != None) \
-                and ('input_one_sequence' not in params or params['input_one_sequence'] == None):
+            if params.get('output_one_name') is not None and params.get('input_one_sequence') is None:
                 raise ValueError('input_one_sequence parameter required if output_one_name parameter is provided')
-            if ('input_one_ref' in params and params['input_one_ref'] != None) \
-                and ('input_one_sequence' in params and params['input_one_sequence'] != None):
+            if params.get('input_one_ref') is not None and params.get('input_one_sequence') is not None:
                 raise ValueError('cannot have both input_one_sequence and input_one_ref parameter')
-            if ('input_one_ref' in params and params['input_one_ref'] != None) \
-                and ('output_one_name' in params and params['output_one_name'] != None):
+            if params.get('input_one_ref') is not None and params.get('output_one_name') is not None:
                 raise ValueError('cannot have both input_one_ref and output_one_name parameter')
-            if ('input_one_ref' not in params or params['input_one_ref'] == None) \
-                and ('input_one_sequence' not in params or params['input_one_sequence'] == None):
+            if params.get('input_one_ref') is None and params.get('input_one_sequence') is None:
                 raise ValueError('input_one_sequence or input_one_ref parameter is required')
 
         return True
@@ -199,8 +186,7 @@ class BlastUtil:
 
         # Write the input_one_sequence to a SequenceSet object
         #
-        if 'input_one_sequence' in params \
-                and params['input_one_sequence'] != None \
+        if params.get('input_one_sequence') is not None \
                 and params['input_one_sequence'] != "Optionally enter DNA sequence...":
 
             #SERVICE_VER = 'release'
@@ -282,8 +268,7 @@ class BlastUtil:
         # SequenceSet
         #
         if query_type_name == 'SequenceSet':
-            if 'input_one_sequence' in params \
-                and params['input_one_sequence'] != None \
+            if params.get('input_one_sequence') is not None \
                 and params['input_one_sequence'] != "Optionally enter DNA sequence..." \
                 and query_type_name != 'SequenceSet':
 
@@ -344,7 +329,7 @@ class BlastUtil:
             DOTFU = KBaseDataObjectToFileUtils (url=self.callbackURL, token=self.ctx['token'], service_ver=SERVICE_VER)
             FeatureSetToFASTA_retVal = DOTFU.FeatureSetToFASTA (FeatureSetToFASTA_params)
             query_fasta_file_path = FeatureSetToFASTA_retVal['fasta_file_path']
-            query_short_id_to_rec_id = FeatureSetToFASTA_retVal['short_id_to_rec_id']
+            #query_short_id_to_rec_id = FeatureSetToFASTA_retVal['short_id_to_rec_id']
             if len(list(FeatureSetToFASTA_retVal['feature_ids_by_genome_ref'].keys())) > 0:
                 appropriate_sequence_found_in_one_input = True
 
@@ -501,7 +486,7 @@ class BlastUtil:
                     elif line.startswith('@'):
                         was_fastq = True
                         header = line[1:]
-                        if last_header != None:
+                        if last_header is not None:
                             new_file_handle.write('>'+last_header)
                             new_file_handle.write(last_seq_buf)
                         last_seq_buf = None
@@ -512,7 +497,7 @@ class BlastUtil:
                         last_line_was_header = False
                     else:
                         continue
-                if last_header != None:
+                if last_header is not None:
                     new_file_handle.write('>'+last_header)
                     new_file_handle.write(last_seq_buf)
 
@@ -967,7 +952,7 @@ class BlastUtil:
         blast_cmd.append('-evalue')
         blast_cmd.append(str(e_value))
         # options (not allowed for format 0)
-        if BLAST_output_format_str != '0' and maxaccepts != None:
+        if BLAST_output_format_str != '0' and maxaccepts is not None:
             blast_cmd.append('-max_target_seqs')
             blast_cmd.append(str(maxaccepts))
 
@@ -1121,11 +1106,11 @@ class BlastUtil:
             header_done = True
             hit_info = line.split("\t")
             sh_hit_seq_id  = hit_info[1]
-            try:
+            if sh_hit_seq_id in target_feature_info['short_id_to_rec_id']:
                 hit_seq_id = target_feature_info['short_id_to_rec_id'][sh_hit_seq_id]
                 hit_info[1] = hit_seq_id
                 line = "\t".join(hit_info)
-            except:
+            else:
                 hit_seq_id = sh_hit_seq_id
             hit_ident      = float(hit_info[2]) / 100.0
             hit_aln_len    = hit_info[3]
@@ -1199,11 +1184,11 @@ class BlastUtil:
 
             output_sequenceSet = dict()
 
-            if 'sequence_set_id' in input_many_sequenceSet and input_many_sequenceSet['sequence_set_id'] != None:
+            if 'sequence_set_id' in input_many_sequenceSet and input_many_sequenceSet['sequence_set_id'] is not None:
                 output_sequenceSet['sequence_set_id'] = input_many_sequenceSet['sequence_set_id'] + "."+search_tool_name+"_Search_filtered"
             else:
                 output_sequenceSet['sequence_set_id'] = search_tool_name+"_Search_filtered"
-            if 'description' in input_many_sequenceSet and input_many_sequenceSet['description'] != None:
+            if 'description' in input_many_sequenceSet and input_many_sequenceSet['description'] is not None:
                 output_sequenceSet['description'] = input_many_sequenceSet['description'] + " - "+search_tool_name+"_Search filtered"
             else:
                 output_sequenceSet['description'] = search_tool_name+"_Search filtered"
@@ -1249,7 +1234,7 @@ class BlastUtil:
                     seq_id = line[1:]  # removes '>'
                     seq_id = pattern.findall(seq_id)[0]
 
-                    if last_seq_id != None:
+                    if last_seq_id is not None:
                         #self.log(console, 'ID: '+last_seq_id)  # DEBUG
                         id_untrans = last_seq_id
                         id_trans = re.sub ('\|',':',id_untrans)  # BLAST seems to make this translation now when id format has simple 'kb|blah' format
@@ -1265,7 +1250,7 @@ class BlastUtil:
                 else:
                     last_seq_buf.append(line)
 
-            if last_seq_id != None:
+            if last_seq_id is not None:
                 #self.log(console, 'ID: '+last_seq_id)  # DEBUG
                 id_untrans = last_seq_id
                 id_trans = re.sub ('\|',':',id_untrans)  # BLAST seems to make this translation now when id format has simple 'kb|blah' format
@@ -1299,7 +1284,7 @@ class BlastUtil:
                     seq_total += 1
 
             output_featureSet = dict()
-            #if 'description' in input_many_featureSet and input_many_featureSet['description'] != None:
+            #if 'description' in input_many_featureSet and input_many_featureSet['description'] is not None:
             #    output_featureSet['description'] = input_many_featureSet['description'] + " - "+search_tool_name+"_Search filtered"
             #else:
             #    output_featureSet['description'] = search_tool_name+"_Search filtered"
@@ -1329,7 +1314,7 @@ class BlastUtil:
         elif target_type_name == 'Genome':
             seq_total = 0
             output_featureSet = dict()
-#            if 'scientific_name' in input_many_genome and input_many_genome['scientific_name'] != None:
+#            if 'scientific_name' in input_many_genome and input_many_genome['scientific_name'] is not None:
 #                output_featureSet['description'] = input_many_genome['scientific_name'] + " - "+search_tool_name+"_Search filtered"
 #            else:
 #                output_featureSet['description'] = search_tool_name+"_Search filtered"
@@ -1357,7 +1342,7 @@ class BlastUtil:
             seq_total = 0
 
             output_featureSet = dict()
-            #if 'description' in input_many_genomeSet and input_many_genomeSet['description'] != None:
+            #if 'description' in input_many_genomeSet and input_many_genomeSet['description'] is not None:
             #    output_featureSet['description'] = input_many_genomeSet['description'] + " - "+search_tool_name+"_Search filtered"
             #else:
             #    output_featureSet['description'] = search_tool_name+"_Search filtered"
@@ -1390,7 +1375,7 @@ class BlastUtil:
         elif target_type_name == 'AnnotatedMetagenomeAssembly':
             seq_total = 0
             output_featureSet = dict()
-#            if 'scientific_name' in input_many_genome and input_many_genome['scientific_name'] != None:
+#            if 'scientific_name' in input_many_genome and input_many_genome['scientific_name'] is not None:
 #                output_featureSet['description'] = input_many_genome['scientific_name'] + " - "+search_tool_name+"_Search filtered"
 #            else:
 #                output_featureSet['description'] = search_tool_name+"_Search filtered"
@@ -1601,7 +1586,7 @@ class BlastUtil:
                             fid_lookup = fid
                             break
                     #self.log (console, "HIT_FID: '"+str(hit_fid)+"' FID_LOOKUP: '"+str(fid_lookup)+"'")  # DEBUG
-                    if fid_lookup == None:
+                    if fid_lookup is None:
                         raise ValueError ("unable to find fid for hit_fid: '"+str(hit_fid))
                     elif fid_lookup not in target_feature_info['feature_id_to_function'][genome_ref]:
                         raise ValueError ("unable to find function for fid: '"+str(fid_lookup))
@@ -1856,7 +1841,7 @@ class BlastUtil:
                                             'name': target_name+'-'+search_tool_name+'_Search-m'+'7'+'.txt',
                                             'label': target_name+'-'+search_tool_name+' Results: m'+'7'})
 
-            if extra_bulk_save_info != None:
+            if extra_bulk_save_info is not None:
                 extension = 'txt'
                 if params['output_extra_format'] == '5':
                     extension = 'xml'
@@ -1917,8 +1902,7 @@ class BlastUtil:
         #
         input_many_refs = params['input_many_refs']
 
-        if 'input_one_sequence' in params \
-                and params['input_one_sequence'] != None \
+        if params.get('input_one_sequence') is not None \
                 and not params['input_one_sequence'].startswith("Optionally enter"):
             input_one_ref = self.objectify_text_query (params, q_seq_type, method_name)
             params['input_one_ref'] = input_one_ref
